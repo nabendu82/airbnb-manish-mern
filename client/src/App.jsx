9bd887d0541ecc4151e8bbea70cf54a601286061
nabendu82 <nabendu.biswas@gmail.com>
2025-01-23 11:36:14 +0530

    Context API done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,22 @@ import Login from './components/Login'
 import Layout from './Layout'
 import Register from './components/Register'
 import axios from 'axios'
+import { UserContextProvider } from './UserContext'
 
 axios.defaults.baseURL = 'http://localhost:4000'
+axios.defaults.withCredentials = true;
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Index />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Index />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,14 +1,18 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { UserContext } from "../UserContext"
 
 const Header = () => {
+    const { user } = useContext(UserContext)
+
     return (
         <header className='p-4 flex justify-between'>
-            <a href="" className='flex items-center gap-1'>
+            <Link to={'/'} className='flex items-center gap-1'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
                 </svg>
                 <span className='font-bold text-xl'>Airbnb</span>
-            </a>
+            </Link>
             <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-md shadow-gray-300'>
                 <div>Anywhere</div>
                 <div className='border-l border-gray-300'></div>
@@ -22,7 +26,7 @@ const Header = () => {
                     </svg>
                 </button>
             </div>
-            <Link to={'/login'} className='flex gap-2 border border-gray-300 rounded-full py-2 px-4'>
+            <Link to={user ? '/account' : '/login'} className='flex gap-2 border border-gray-300 rounded-full py-2 px-4'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
@@ -31,6 +35,9 @@ const Header = () => {
                         <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
                     </svg>
                 </div>
+                {!!user && (
+                    <div>{user.name}</div>
+                )}
             </Link>
         </header>
     )
